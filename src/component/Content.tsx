a3cbbe4939a1238fdd4ba0656c67ae75802e04db
sheffey <gxf121024@outlook.com>
2023-12-07 00:25:33 +0800

    fix: path ui refresh

--- src/component/Content.tsx
+++ src/component/Content.tsx
@@ -18,63 +18,62 @@ const Content: VFC<{ serverAPI: ServerAPI }> = ({ serverAPI }) => {
   const [showPath, setShowPath] = useState('/');
 
   useEffect(() => {
-      Backend.initialize(serverAPI);
+    Backend.initialize(serverAPI);
 
-      (async () => {
-          const settings = await SettingsManager.loadFromFile();
-          setShowPath(settings.initPath);
-      })();
+    (async () => {
+      const settings = await SettingsManager.loadFromFile();
+      setShowPath(settings.cheatPath);
+    })();
   }, []);
 
   const handleBrowse = async () => {
-      logger.info("Original showpath is: " + showPath);
-      const filePickerResult = await Backend.openFilePicker("/home/deck/Games", ["exe"]);
+    logger.info("Original showpath is: " + showPath);
+    const filePickerResult = await Backend.openFilePicker("/home/deck/Games", ["exe"]);
 
-      logger.info("Selected path is: " + filePickerResult.path);
-      setShowPath(filePickerResult.path)
-      await SettingsManager.saveToFile({ cheatPath: filePickerResult.path })
+    logger.info("Selected path is: " + filePickerResult.path);
+    setShowPath(filePickerResult.path)
+    await SettingsManager.saveToFile({ cheatPath: filePickerResult.path })
   };
 
   useEffect(() => {
-      logger.info("Noticed showpath changes to: " + showPath);
+    logger.info("Noticed showpath changes to: " + showPath);
   }, [showPath]);
 
   return (
-      <PanelSection title="setting">
+    <PanelSection title="setting">
 
-          <PanelSectionRow>
-              <ButtonItem
-                  layout="below"
-                  onClick={handleBrowse}
-              >
-                  Select
-              </ButtonItem>
-              <DialogBody>
-                  <p>path: {showPath}</p>
-              </DialogBody>
-              {/* <ShowPath path={showPath} /> */}
+      <PanelSectionRow>
+        <ButtonItem
+          layout="below"
+          onClick={handleBrowse}
+        >
+          Select
+        </ButtonItem>
+        <DialogBody>
+          <p>path: {showPath}</p>
+        </DialogBody>
 
-          </PanelSectionRow>
+      </PanelSectionRow>
 
-          <PanelSectionRow>
-              <ButtonItem
-                  layout="below"
-                  onClick={(e: { currentTarget: any; }) =>
-                      showContextMenu(
-                          <Menu label="Menu" cancelText="CAAAANCEL" onCancel={() => { }}>
-                              <MenuItem onSelected={() => { }}>Item #1</MenuItem>
-                              <MenuItem onSelected={() => { }}>Item #2</MenuItem>
-                              <MenuItem onSelected={() => { }}>Item #3</MenuItem>
-                          </Menu>,
-                          e.currentTarget ?? window
-                      )
-                  }
-              >
-                  Menu
-              </ButtonItem>
-          </PanelSectionRow>
+      <PanelSectionRow>
+        <ButtonItem
+          layout="below"
+          onClick={(e: { currentTarget: any; }) =>
+            showContextMenu(
+              <Menu label="Menu" cancelText="CAAAANCEL" onCancel={() => { }}>
+                <MenuItem onSelected={() => { }}>Item #1</MenuItem>
+                <MenuItem onSelected={() => { }}>Item #2</MenuItem>
+                <MenuItem onSelected={() => { }}>Item #3</MenuItem>
+              </Menu>,
+              e.currentTarget ?? window
+            )
+          }
+        >
+          Menu
+        </ButtonItem>
+      </PanelSectionRow>
 
-      </PanelSection>
+    </PanelSection>
   );
 };
 
