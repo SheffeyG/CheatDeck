755ed7f069ef2df4356bb262fa72afb7c006e2aa
sheffey <57262511+SheffeyG@users.noreply.github.com>
2024-01-24 01:16:32 +0800

    Add shared prefix command

--- src/views/Advanced.tsx
+++ src/views/Advanced.tsx
@@ -1,7 +1,9 @@
 import {
   AppDetails,
   DialogButton,
+  Field,
   Focusable,
+  TextField,
   ToggleField,
 } from "decky-frontend-lib"
 import { VFC, useEffect, useState } from "react"
@@ -9,15 +11,18 @@ import { VFC, useEffect, useState } from "react"
 // import logger from "../utils/logger"
 import { Backend } from "../utils/backend";
 import { Options } from "../utils/options";
+import { FaFolderOpen } from "react-icons/fa";
 
 const Advanced: VFC<{ appid: number }> = ({ appid }) => {
   const [options, setOptions] = useState(new Options(""));
   const [isSteam, setIsSteam] = useState(true);
+  const [showPrefix, setShowPrefix] = useState(false);
 
   useEffect(() => {
     const { unregister } = SteamClient.Apps.RegisterForAppDetails(appid, (detail: AppDetails) => {
       const optionsString = detail.strLaunchOptions;
       const savedOptions = new Options(optionsString);
+      setShowPrefix(savedOptions.hasOption('STEAM_COMPAT_DATA_PATH'));
       setOptions(savedOptions);
       if (optionsString.match("heroicgameslauncher") || optionsString.match("Emulation")) {
         setIsSteam(false);
@@ -25,6 +30,16 @@ const Advanced: VFC<{ appid: number }> = ({ appid }) => {
     })
     setTimeout(() => { unregister() }, 1000);
   }, [])
+
+  const handleBrowse = async () => {
+    const prefixDir = options.getOptionValue('STEAM_COMPAT_DATA_PATH');
+    const defaultDir = prefixDir ? prefixDir : "/home/deck";
+    const filePickerRes = await Backend.openFilePicker(defaultDir);
+    const prefixPath = filePickerRes.path;
+    const newOptions = new Options(options.getOptionsString());
+    newOptions.setOptionValue('STEAM_COMPAT_DATA_PATH', `"${prefixPath}"`);
+    setOptions(newOptions);
+  };
 
   const saveOptions = () => {
     if (isSteam) {
@@ -63,6 +78,60 @@ const Advanced: VFC<{ appid: number }> = ({ appid }) => {
           setOptions(updatedOptions);
         }}
       />
+
+      <ToggleField
+        label="Shared Prefix"
+        description='Specify prefix for the game (include saved game)'
+        bottomSeparator={"none"}
+        checked={showPrefix}
+        onChange={(enable: boolean) => {
+          setShowPrefix(enable);
+          if (!enable) {
+            const updatedOptions = new Options(options.getOptionsString());
+            updatedOptions.setOptionValue('STEAM_COMPAT_DATA_PATH', '');
+            setOptions(updatedOptions);
+          }
+        }}
+      />
+      {showPrefix && (<Field
+        key={1}
+        label={"Prefix path"}
+        padding={"none"}
+        bottomSeparator="thick"
+      >
+        <Focusable
+          style={{
+            boxShadow: "none",
+            display: "flex",
+            justifyContent: "right",
+            padding: "10px 0",
+          }}
+        >
+          <TextField
+            style={{
+              padding: "10px",
+              fontSize: "14px",
+              width: "400px",
+            }}
+            disabled={true}
+            value={options.getOptionValue('STEAM_COMPAT_DATA_PATH')}
+          />
+          <DialogButton
+            onClick={handleBrowse}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              padding: "10px",
+              maxWidth: "40px",
+              minWidth: "auto",
+              marginLeft: ".5em",
+            }}
+          >
+            <FaFolderOpen />
+          </DialogButton>
+        </Focusable>
+      </Field>)}
 
       <DialogButton
         onClick={saveOptions}
