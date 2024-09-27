776f69c526b32733510fdf0933b1bd5e071cc2b5
eXhumer <exhumer@exhumer.cc>
2024-09-27 03:56:33 -0600

    chore: Remove unused backend methods
    * add CI job to lint backend code
    
    Signed-off-by: eXhumer <exhumer@exhumer.cc>

--- main.py
+++ main.py
@@ -10,36 +10,21 @@ loggingDir = decky.DECKY_PLUGIN_LOG_DIR
 logger = decky.logger
 
 # Setup backend logger
-logger.setLevel(logging.DEBUG)  # can be changed to logging.DEBUG for debugging issues
+logger.setLevel(logging.DEBUG)
 logger.info('[backend] Settings path: {}'.format(settingsDir))
 settings = SettingsManager(name="settings", settings_directory=settingsDir)
 settings.read()
 
 
 class Plugin:
-    @classmethod
-    async def plugin_info(cls):
-        # Call plugin_info only once preferably
-        logger.debug('[backend] PluginInfo:\n\tPluginName: {}\n\tPluginVersion: {}\n\tDeckyVersion: {}'.format(
-            decky.DECKY_PLUGIN_NAME,
-            decky.DECKY_PLUGIN_VERSION,
-            decky.DECKY_VERSION,
-        ))
-        pluginInfo = {
-            "name": decky.DECKY_PLUGIN_NAME,
-            "version": decky.DECKY_PLUGIN_VERSION
-        }
-        return pluginInfo
+    async def _main(self):
+        logger.info("[backend] Loading CheatDeck!")
 
-    @classmethod
-    async def logger(cls, logLevel: str, msg: str):
-        msg = '[frontend] {}'.format(msg)
-        match logLevel.lower():
-            case 'info':      logger.info(msg)
-            case 'debug':     logger.debug(msg)
-            case 'warning':   logger.warning(msg)
-            case 'error':     logger.error(msg)
-            case 'critical':  logger.critical(msg)
+    async def _unload(self):
+        logger.info("[backend] Unloading CheatDeck!")
+
+    async def _uninstall(self):
+        logger.info("[backend] Uninstalling CheatDeck!")
 
     @classmethod
     async def settings_read(cls):
