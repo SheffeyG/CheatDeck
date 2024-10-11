e5d45583ca09b006afe84bdff3353b6a71e9be6c
sheffey <57262511+SheffeyG@users.noreply.github.com>
2024-10-11 16:14:31 +0800

    closure! no useMemo

--- src/utils/translate.ts
+++ src/utils/translate.ts
@@ -1,29 +1,30 @@
-import { useMemo } from "react";
+import logger from "./logger";
 
 import * as en from "../data/i18n/en.json";
 import * as zhCn from "../data/i18n/zh-cn.json";
-import logger from "./logger";
 
 type Language = { [key: string]: string };
 type Languages = { [key: string]: Language };
 
-const languages: Languages = { zhCn, en };
+const languages: Languages = { en, zhCn };
 
 function getCurrentLangCode(): string {
   const steamLang = window.LocalizationManager.m_rgLocalesToUse[0];
-  const langCode = steamLang.replace(/-([a-z])/g, (_, letter: string) =>
-    letter.toUpperCase(),
+  const langCode = steamLang.replace(
+    /-([a-z])/g, (_, letter: string) => letter.toUpperCase(),
   );
   logger.info(`LanguageCode: ${langCode}`);
   return langCode;
 }
 
 function translate() {
-  const langCode: string = useMemo(() => getCurrentLangCode(), []);
+  const langCode: string = getCurrentLangCode();
   const lang: Language = languages[langCode] ?? languages.en;
   return function (label: string, defaultString: string): string {
     return lang[label] ?? defaultString;
   };
 }
 
-export default translate;
+const t = translate();
+
+export default t;
