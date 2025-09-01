e41969afc0e905def6a2c2251051cf0a082d3051
SheffeyG <57262511+SheffeyG@users.noreply.github.com>
2025-09-01 16:01:16 +0800

    refactor: move patch to root

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { routerHook } from "@decky/api";
 import { definePlugin, staticClasses } from "@decky/ui";
 import { FaWrench as PluginIcon } from "react-icons/fa";
 
-import contextMenuPatch, { LibraryContextMenu } from "./utils/patch";
+import contextMenuPatch, { LibraryContextMenu } from "./patch";
 import Content from "./views/Content";
 import PageRouter from "./views/PageRouter";
 
