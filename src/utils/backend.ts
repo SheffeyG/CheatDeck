4300a0de4af0c479ba52a4e273b23c142e487c90
SheffeyG <57262511+SheffeyG@users.noreply.github.com>
2025-10-19 11:03:09 +0800

    revert: "refactor: simplify backend function call"
    This reverts commit a8dadcf2932d7c1bcbae6531ec67bd0c52de0490.

--- src/utils/backend.ts
+++ src/utils/backend.ts
@@ -1,25 +1,44 @@
-import { call } from "@decky/api";
+import { callable } from "@decky/api";
 import { v4 as uuid } from "uuid";
 
+/* Backend bridge functions */
+
+async function getEnv(env: string) {
+  const callback = callable<[string], string>("get_env");
+  return await callback(env);
+}
+
+async function getSetting(key: string, defaults: unknown) {
+  const callback = callable<[{ key: string; defaults: unknown }], unknown>("get_setting");
+  return await callback({ key, defaults });
+}
+
+async function setSetting(key: string, value: unknown) {
+  const callback = callable<[{ key: string; value: unknown }], unknown>("set_setting");
+  return await callback({ key, value });
+}
+
+/* Frontend exported functions */
+
 export const getHomePath = async (): Promise<string> => {
-  return await call("get_env", "DECKY_USER_HOME");
+  return await getEnv("DECKY_USER_HOME");
 };
 
 export const getCustomOptions = async (): Promise<CustomOption[]> => {
-  const savedOpt = await call("get_settings", "CustomOptions", []) as CustomOption[];
+  const savedOpt = await getSetting("CustomOptions", []) as CustomOption[];
   const optsWithId = savedOpt.map(option => ({ ...option, id: uuid() }));
   return optsWithId;
 };
 
 export const setCustomOptions = async (data: CustomOption[]): Promise<void> => {
   const optsWithoutId = data.map(({ id, ...rest }) => rest);
-  await call("set_settings", "CustomOptions", optsWithoutId);
+  await setSetting("CustomOptions", optsWithoutId);
 };
 
 export const getShowPreview = async (): Promise<boolean> => {
-  return await call("get_settings", "ShowPreview", false) as boolean;
+  return await getSetting("ShowPreview", false) as boolean;
 };
 
 export const setShowPreview = async (value: boolean): Promise<void> => {
-  await call("set_settings", "ShowPreview", value);
+  await setSetting("ShowPreview", value);
 };
