c3ee7291b3d83773f89716e1bdabed8e32aeafc8
SheffeyG <57262511+SheffeyG@users.noreply.github.com>
2025-12-24 10:00:35 +0800

    fix: handle close modal correctly. Fix #69

--- src/modals/QrCode.tsx
+++ src/modals/QrCode.tsx
@@ -2,14 +2,15 @@ import { ModalRoot } from "@decky/ui";
 import { QRCodeSVG } from "qrcode.react";
 import { FC } from "react";
 
-export const defaultUrl = "https://github.com/SheffeyG/CheatDeck";
-
-export const QrCode: FC<{ url?: string }> = ({ url = defaultUrl }) => (
-  <ModalRoot onClose={() => { }}>
+export const QrCode: FC<{
+  closeModal?: () => void;
+  url: string;
+}> = ({ closeModal = () => { }, url }) => (
+  <ModalRoot closeModal={closeModal}>
     <QRCodeSVG
       style={{ margin: "0 auto 1.5em auto" }}
-      value={url || " "}
-      includeMargin
+      value={url || ""}
+      marginSize={4}
       size={256}
     />
     <span style={{ textAlign: "center", wordBreak: "break-word" }}>
