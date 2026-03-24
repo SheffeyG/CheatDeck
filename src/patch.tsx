3fad9f8cb50b84826cef1a459ac160a2b6411d29
SheffeyG <57262511+SheffeyG@users.noreply.github.com>
2026-03-24 09:32:39 +0800

    fix: crash in beta client
    https://github.com/SteamGridDB/decky-steamgriddb/commit/7168307beb91c336a65899fa63c38f96f8b4624c

--- src/patch.tsx
+++ src/patch.tsx
@@ -144,7 +144,6 @@ export const LibraryContextMenu = fakeRenderComponent(
   Object.values(
     findModuleByExport((e: Export) => e?.toString && e.toString().includes('().LibraryContextMenu'))
   ).find((sibling) => (
-    sibling?.toString().includes('createElement') &&
     sibling?.toString().includes('navigator:')
   )) as FC
 ).type;
